9a00bf974c9f57f5d80aaf717e61fd2f762460ac
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-05-15 23:27:04 +0600

    file add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const image = [...document.querySelectorAll(".image")];
+const PopUp_layout = document.querySelector(".PopUp_layout");
+const crossBtn = document.querySelector(".crossBtn");
+const boroImg = document.querySelector(".boroImg");
+
+image.forEach((oneImage)=>{
+    oneImage.addEventListener("click", (event) => {
+        imageChanger(event.target.src);
+        PopUp_layout.classList.add("active");
+    });
+});
+
+crossBtn.addEventListener(`click`, ()=>{
+    PopUp_layout.classList.remove("active");
+});
+
+// update image
+function imageChanger (ImgUrl) {
+    let boroImgUrl =ImgUrl.split("/").splice(6).join("/");
+    boroImg.src = boroImgUrl;
+};  
